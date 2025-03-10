37f291d6af7611636cf5bb71023c3a988463feb7
Bruno <themasteremailsl@gmail.com>
2025-03-10 16:17:42 -0300

    style: increase margin for Services section title

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -37,7 +37,7 @@ const Services = () => {
     <section className="w-full py-16 px-6 md:px-16 lg:px-20 bg-white">
       <div className="max-w-7xl mx-auto">
         {/* Services Title */}
-        <div className="flex flex-row align-center mb-6 gap-8">
+        <div className="flex flex-row align-center mb-24 gap-8">
           <h2 className="text-3xl md:text-4xl font-bold px-4 py-2 bg-lime-300 rounded-lg">Services</h2>
           <p className="text-lg max-w-3xl">
             At our digital marketing agency, we offer a range of services to
